15ef182355a92f07c4adc5671c86902a2fa0383d
dwegrzyn <dwegrzyn@udel.edu>
2017-01-27 16:05:44 +1300

    Filter stuff :/
    Put add and grayscale in there with blur, but it doesn’t seem to be
    showing anything visually when they are combined

--- js/filterState.js
+++ js/filterState.js
@@ -3,7 +3,7 @@ var imageState = {
   create: function(){
     //format for shader list: [[shader, textName, passes]
     //note: passes removed for now bc not working
-    var puzzle = new filterClass(game, 'turtlePic1', [[blurShader, "BLUR",0], [blurShader,"COOLBLUR",1], [blurShader, "TUMBLUR",2]]);
+    var puzzle = new filterClass(game, 'turtlePic1', [[blurShader, "BLUR",0], [arithmeticAddShader,"ADD",1], [grayscaleShader, "GRAYSCALE",2]]);
     //var puzzle = new filterClass(game, 'turtlePic1', [[blurShader, "BLUR", 1]])
     puzzle.setup();
 
