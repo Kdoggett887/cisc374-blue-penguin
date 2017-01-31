5ba41a508a1d4e7cfb06defcc66c3168fa0ea711
kilnas <kalina@udel.edu>
2017-01-30 21:38:07 -0500

    remove unused asset

--- js/Level2.js
+++ js/Level2.js
@@ -15,7 +15,7 @@ var Level2 = {
 
     // Setup Player
     if(TA.level2.startingLevel){
-      TA.level2.turtleRed = new Turtle(game.world.centerX - 650, game.world.centerY - 200, game, 'turtle', sampleText, [[removeRedShader, "RED", 1],[grayscaleShader, "GRAYSCALE", 0], [arithmeticAddShader,"ADD", 0]]);
+      TA.level2.turtleRed = new Turtle(game.world.centerX - 650, game.world.centerY - 200, game, 'turtle', level2RedText, [[removeRedShader, "RED", 1],[grayscaleShader, "GRAYSCALE", 0], [arithmeticAddShader,"ADD", 0]]);
       //TA.level2.turtleGreen = new Turtle(game.world.centerX + 400, game.world.centerY + 100, game, 'turtle', sampleText, [[removeGreenShader, "GREEN", 1]]);
       //TA.level2.turtleBlue = new Turtle(game.world.centerX - 650, game.world.centerY + 400, game, 'turtle', sampleText, [[removeBlueShader, "BLUE", 1]]);
       //TA.level2.turtleFinal = new Turtle(game.world.centerX, game.world.centerY - 800, game, 'turtle', sampleText, [[removeRedShader, "RED", 1], [removeGreenShader, "GREEN", 1], [removeBlueShader, "BLUE", 0]]);
